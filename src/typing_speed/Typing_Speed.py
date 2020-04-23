b831cdf77e83e1d962e39671d0ca17592c76c1a9
naman <naman.gupta_cs18@gla.ac.in>
2020-04-23 21:18:41 +0530

    completed check_password() method

--- src/typing_speed/Typing_Speed.py
+++ src/typing_speed/Typing_Speed.py
@@ -25,6 +25,56 @@ def check_Email():
         return False
 
 
+def check_password():
+    input_password = input("Enter the password: ")
+    s = input_password
+    special = ",.#@$%&^*_-"
+    flag = 1
+    l = len(s)
+    if any(i.isspace() for i in s):
+        print("Inavalid Password!!")
+        print("There shouldn't be any space!!")
+        return False
+
+
+    elif not (l >= 5):
+        print("Inavalid Password!!")
+        print("Password must contain atleast five characters!!")
+        return False
+
+
+    elif not (l <= 10):
+        print("Inavalid Password!!")
+        print("Password contains atmost ten characters!!")
+        return False
+
+
+    elif not (any(i.isdigit() for i in s)):
+        print("Inavalid Password!!")
+        print("Password must contains atleast one digit!!")
+        return False
+
+    elif not (any(i in special for i in s)):
+        print("Inavalid Password!!")
+        print("Password must contain atleast one special character!!")
+        return False
+
+    elif not (any(i.isupper() for i in s)):
+        print("Inavalid Password!!")
+        print("Password must contain atleast one upper case alphabet!!")
+        return False
+
+
+    elif not (any(i.islower() for i in s)):
+        print("Inavalid Password!!")
+        print("Password must contain atleast one lower case alphabet!!")
+        return False
+
+
+    else:
+        return "True"
+
+
 if choice == 1:
     while True:
         if check_Email():
